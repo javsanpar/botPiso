89175f78e0959faeca559d2eefc318905e284c48
piso <piso@serverDebian.piso.com>
2017-02-10 09:02:45 +0100

    change ip

--- personasNmap.rb
+++ personasNmap.rb
@@ -1,4 +1,4 @@
-value = %x( nmap -sP -n 192.168.43.0/24 )
+value = %x( nmap -sP -n 192.168.1.0/24 )
 
 knownMacs = Hash.new
 macs = Array.new
@@ -7,7 +7,7 @@ value = value.split("MAC Address: ")
 value.delete_at(0)
 knownMacs["Javi"] = "74:23:44:E8:06:16"
 knownMacs["Pablo"] = "10:2A:B3:EB:DE:77"
-knownMacs["Alberto"] = "F8:E8:79:12:86:EC"
+knownMacs["Alberto"] = "F8:E0:79:12:86:EC"
 knownMacs["Dani"] = "68:3E:34:08:F2:D6"
 
 value.each do |i|
